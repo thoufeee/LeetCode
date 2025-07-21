49485ea8bed14391f586da6c9c35e832ce9646cc
Thoufeeq <connectthoufeeq@gmail.com>
2025-07-21 09:47:06 +0530

    Time: 0 ms (100%), Space: 9.6 MB (86.66%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,17 @@
+func maxProfit(prices []int) int {
+    result := 0;
+    if len(prices) == 1 {
+        return result;
+    }
+	min := prices[0]
+    for _, val := range prices {
+		if val < min {
+			min = val
+		}
+	    profi := val - min
+       if profi > result {
+	        result = profi
+       }
+    }
+          return result
+}
