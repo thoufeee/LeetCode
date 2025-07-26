626ccdf95898d5c1311f1d3ce5ed9f597c89431b
Thoufeeq <connectthoufeeq@gmail.com>
2025-07-26 11:22:04 +0530

    Time: 13 ms (5.71%), Space: 8.9 MB (5.71%) - LeetHub

---
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.go
@@ -0,0 +1,21 @@
+func reverseDegree(s string) int {
+    
+    ma := make(map[int]int)
+
+	for i, val := range s {
+
+		if val >= 'a' && val <= 'z' {
+			degree := 26 - int(val-'a')
+
+			ma[i+1] = degree
+		}
+	}
+
+	result := 0
+
+	for i, val := range ma {
+		result += i * val
+	}
+
+        return result
+}
