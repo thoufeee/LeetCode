4b6e60776b062cee713aec173322de8f5e9e84a0
Thoufeeq <connectthoufeeq@gmail.com>
2025-08-23 19:50:33 +0530

    Time: 0 ms (100%), Space: 4.1 MB (32.05%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
@@ -10,12 +10,11 @@ func preorderTraversal(root *TreeNode) []int {
          if root == nil {
               return [] int {}
          }
-
          arr := [] int {}
          
          arr = append(arr, root.Val)
          arr = append(arr, preorderTraversal(root.Left)...)
          arr = append(arr, preorderTraversal(root.Right)...)
-
+         
          return  arr
 }
