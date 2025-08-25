03e609165dabdc53a7068ff6fbf7d5fcd4414c2d
Thoufeeq <connectthoufeeq@gmail.com>
2025-08-25 12:55:18 +0530

    Time: 0 ms (100%), Space: 11.3 MB (20.49%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -0,0 +1,59 @@
+
+// func Sub(arr []int, n int, res *[][] int){
+//        if n == len(arr) {
+//            return 
+//        }
+
+//        for i:=n;i<len(arr);i++ {
+//              sub := make([]int, i-n+1)
+//               copy(sub, arr[n:i+1])
+//               *res = append(*res, sub)
+//        }
+
+//        Sub(arr,n+1,res)
+// }
+
+
+func maxSubArray(nums []int) int {
+
+    // if len(nums) == 1 {
+    //       return nums[0]
+    // }
+    
+    //    res := [][]int{}
+
+    //    Sub(nums,0,&res)
+
+    //    result := -100000
+
+    //    for _,val := range res {
+    //         sum := 0;
+    //         for _,va := range val {
+    //                sum += va;
+    //         }
+
+    //         if sum > result {
+    //               result = sum
+    //         }
+    //    }  
+
+    //      return result  
+
+
+        max := nums[0]
+        result := nums[0]
+
+        for i:=1;i<len(nums);i++ {
+               if max + nums[i] > nums[i] {
+                        max = max + nums[i]
+               }else {
+                     max = nums[i]
+               }
+
+               if max > result {
+                    result = max
+               }
+        }
+
+           return result
+}
