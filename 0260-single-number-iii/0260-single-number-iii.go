f16dbb7b7e2102df34d70c905d05f29b7d39a094
Thoufeeq <connectthoufeeq@gmail.com>
2025-08-30 19:44:33 +0530

    Time: 0 ms (100%), Space: 6.3 MB (14.67%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.go
@@ -0,0 +1,15 @@
+func singleNumber(nums []int) []int {
+    hash := make(map[int]int)
+
+	result := []int{}
+	for _, val := range nums {
+		hash[val]++
+	}
+
+	for i, val := range hash {
+		if val < 2 {
+			result = append(result, i)
+		}
+	}
+        return result
+}
