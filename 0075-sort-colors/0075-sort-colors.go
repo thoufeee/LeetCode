99295e29c5781bf703935e649ce51faa9e444097
Thoufeeq <connectthoufeeq@gmail.com>
2025-09-01 12:50:32 +0530

    Time: 0 ms (100%), Space: 4.2 MB (11.1%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.go
@@ -0,0 +1,30 @@
+
+func Partition(arr []int, low int, high int) int {
+	pivot := arr[low]
+
+	i := low + 1
+
+	for j := low + 1; j <= high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+
+	arr[i-1], arr[low] = arr[low], arr[i-1]
+
+	return i - 1
+}
+
+func Quick(arr []int, low int, high int) {
+	if low < high {
+		p := Partition(arr, low, high)
+		Quick(arr, low, p-1)
+		Quick(arr, p+1, high)
+	}
+}
+
+func sortColors(nums []int)  {
+      
+        Quick(nums,0,len(nums)-1)
+}
