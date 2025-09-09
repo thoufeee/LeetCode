b5bb36d03b30cf72d4f86bf3a05471eb27e13702
Thoufeeq <connectthoufeeq@gmail.com>
2025-09-09 09:45:57 +0530

    Time: 3 ms (11.96%), Space: 6.7 MB (17.39%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.go
+++ 3194-find-words-containing-character/3194-find-words-containing-character.go
@@ -4,7 +4,7 @@ func findWordsContaining(words []string, x byte) []int {
 	for i, val := range words {
 
 		for j := 0; j < len(val); j++ {
-			if string(val[j]) == string(x) {
+			if val[j] == x {
 				res = append(res, i)
 				break
 			}
