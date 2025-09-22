d67a9d0e4ed89ebab3e7a431ebb61fb18e8d05a8
Thoufeeq <connectthoufeeq@gmail.com>
2025-09-22 10:10:44 +0530

    Time: 2 ms (16.28%), Space: 4.3 MB (83.72%) - LeetHub

---
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.go
@@ -0,0 +1,26 @@
+func maxFrequencyElements(nums []int) int {
+    
+    hash := make(map[int]int)
+
+	max := 0
+
+	result := 0
+
+	for _, val := range nums {
+		hash[val]++
+	}
+
+	for _, val := range hash {
+		if val > max {
+			max = val
+		}
+	}
+
+	for _, val := range hash {
+		if val == max {
+			result += val
+		}
+	}
+
+    return result
+}
