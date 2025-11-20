c97b9b97fe527dc79f7fc0b744abe38ca61681c0
Thoufeeq <connectthoufeeq@gmail.com>
2025-11-20 15:46:34 +0530

    Time: 48 ms (25.37%), Space: 14.4 MB (28.11%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -2,21 +2,18 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 
     result := false
 
-	hash := make(map[int]*int)
+	hash := make(map[int]int)
 
 	for i, val := range nums {
-		if err := hash[val]; err == nil {
-			hash[val] = &i
+		if _,err := hash[val]; !err {
+			hash[val] = i
 		} else {
 			a := hash[val]
-
-			// fmt.Println(i, int(*a))
-			b := i - int(*a)
-			// fmt.Println(b)
+			b := i - a
 			if b <= k {
 				result = true
 			}
-			hash[val] = &i
+			hash[val] = i
 		}
 
 	}
