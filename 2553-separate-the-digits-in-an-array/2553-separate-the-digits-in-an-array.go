0972487c66844c860974de171cf64ab2c3f802c3
Thoufeeq <connectthoufeeq@gmail.com>
2025-11-23 20:30:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (56%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.go
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.go
@@ -4,11 +4,12 @@ func separateDigits(nums []int) []int {
 
 	for _, val := range nums {
 		s := strconv.Itoa(val)
-
 			s2 := strings.Split(s, "")
 			for _, n := range s2 {
-				n2, _ := strconv.Atoi(n)
+				n2, err := strconv.Atoi(n)
+                if err == nil {
 				res = append(res, n2)
+                }
 			}
 	}
 
