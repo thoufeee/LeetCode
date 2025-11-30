7487b44cee3f81af12846e6fe0b376805dd46c14
Thoufeeq <connectthoufeeq@gmail.com>
2025-11-30 12:23:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (57.14%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.go
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.go
@@ -2,8 +2,8 @@ func minOperations(nums []int, k int) int {
 
     sum := 0
 
-    for _,val := range nums {
-           sum += val
+    for _,v := range nums {
+           sum += v
     }
 
         return sum % k
